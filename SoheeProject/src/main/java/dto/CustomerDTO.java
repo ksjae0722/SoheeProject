78f74f48094e92a411059129c2b9854f5b528a34
sh253 <sh253@DESKTOP-JO70R4R>
2023-07-14 00:10:07 +0900

    [강소희] 고객/상품DTO 생성

---
+++ SoheeProject/src/main/java/dto/CustomerDTO.java
@@ -0,0 +1,9 @@
+package main.java.dto;
+
+public class CustomerDTO {
+	String userID;			//사용자 아이디
+	String userPassword;	//사용자 비밀번호
+	String userName;		//사용자 이름
+	String address;			//사용자 주소
+	String phoneNumber;		//사용자 전화번호
+}
---
+++ SoheeProject/src/main/java/dto/ProductDTO.java
@@ -0,0 +1,8 @@
+package main.java.dto;
+
+public class ProductDTO {
+	String productName;		//제픔명
+	int price;				//가격
+	int stock;				//재고
+	String description;		//제품설명
+}
